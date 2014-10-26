afbeca4cf730721470fcf88b064c0519bf9538d8
mjg <michael.glatzmaier@gmail.com>
2014-10-26 12:26:43 -0500

    Added piece panel, draws one piece to panel.
    Signed-off-by: mjg <michael.glatzmaier@gmail.com>

--- src/graphics/BlokusWindow.java
+++ src/graphics/BlokusWindow.java
@@ -57,11 +57,22 @@ public class BlokusWindow extends JFrame
 				BoardPanel.add(new DrawMainBoard(nBoxesX, nBoxesY));
 
 				wPiecePanel = new JPanel();
+				wPiecePanel.add(new DrawWhitePiecePanel(nBoxesX, nBoxesY));
+				
 				bPiecePanel = new JPanel();
 				GameDataPanel = new JPanel();
 				
 				
-				MainFrame.add(BoardPanel);
+				//MainFrame.add(BoardPanel);
+				//MainFrame.add(wPiecePanel);
+				
+				MainFrame.setResizable(false);
+				MainFrame.setLayout(new BorderLayout());
+				
+				MainFrame.add(BoardPanel, BorderLayout.CENTER);
+				MainFrame.add(wPiecePanel,BorderLayout.SOUTH);
+				//MainFrame.setLayout(new GridLayout(1, 2));
+				
 				MainFrame.pack();
 				MainFrame.setVisible(true);
 				MainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -127,6 +138,62 @@ public class BlokusWindow extends JFrame
 					locX = ( offsetX + (int)boxSizeX * x);
 					g.drawRect(locX, locY, (int)boxSizeX, (int)boxSizeY);
 				}
+			}
+		}
+	}
+	
+	
+
+	// should eventually use nPieces and piece geometry to set the size of this panel
+	public class DrawWhitePiecePanel extends JComponent
+	{
+		
+		private float boxSizeY = -1;
+		private float boxSizeX = -1;
+		private int nBloksX = -1;
+		private int nBloksY = -1;
+		
+		DrawWhitePiecePanel(int _nBoxesX, int _nBoxesY)
+		{		
+			nBloksX = _nBoxesX;
+			nBloksY = _nBoxesY;
+			
+			boxSizeY = (float) (  BoardPanelHeight / 1.5 ) / _nBoxesY;
+			boxSizeX = (float) (  BoardPanelWidth / 1.5 ) / _nBoxesX;
+			
+			setPreferredSize(new Dimension(nBloksX * (int)boxSizeX, 6*(int)boxSizeY));
+			getContentPane().setBackground(Color.white);
+		}
+		
+		@Override
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g);
+	        setOpaque(true);
+
+			g.setColor(Color.white);
+			g.fillRect(0,0, getWidth(), getHeight());
+			
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setStroke(new BasicStroke((float) 3.5));
+			g2.setColor(Color.black);
+			g2.drawRect(0,0,getWidth(),getHeight());
+
+			int offsetX = (int) boxSizeX / 3; 
+			int offsetY = (int) boxSizeY / 3;
+			
+			int locX = 0;
+			int locY = (offsetY + (int) boxSizeY);
+			
+			for( int x = 0; x<3; ++x)
+			{
+				g.setColor(Color.black);
+				locX = ( offsetX + x*(int)boxSizeX/3);
+				locY = offsetY;
+				g.drawRect(locX,locY,(int)boxSizeX/3, (int)boxSizeY/3);
+				g.setColor(Color.cyan);
+				g.fillRect(locX,locY, (int)boxSizeX/3,(int)boxSizeY/3);
+
 			}
 		}
 	}
